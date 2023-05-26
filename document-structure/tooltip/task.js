45507d04854396821b8b975dd07b3e71ba41c1cb
pevec1 <pevec1@yandex.ru>
2023-05-26 19:08:57 +0300

    работаю над этой задачей

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,26 @@
 let divs = document.querySelectorAll('.has-tooltip')
 let elem = document.createElement('div')
 
-let elems = document.querySelectorAll('.tooltip')
-for (elem of elems) {
-  if (elem.classList.contains('tooltip_active')) {
-    elem.classList.remove('tooltip_active')
-  }
-}
 for (let div of divs) {
-  div.onfocus = () => {
+  div.addEventListener('focus', () => {
     div.addEventListener('click', e => {
       e.preventDefault()
       elem.classList.add('tooltip')
       elem.innerHTML = div.getAttribute('title')
       let w = window.getComputedStyle(div).width
-      elem.style = 'display: inline-block; position: absolute; margin-left: -100px; margin-top: -30px'
+      elem.style = 'left: 0; top: 0'
       div.parentNode.insertBefore(elem, div.nextSibling)
       elem.classList.add('tooltip_active')
+      console.log('tooltip active')
+      let isAgain = true
+      div.addEventListener('click', e => {
+        e.preventDefault()
+        if (isAgain) {
+          elem.classList.remove('tooltip_active')
+          console.log('tooltip remove')
+          isAgain = false
+        }
+      })
     })
-  }
+  })
 }
