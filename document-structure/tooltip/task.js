d7365bbfc9c471204a523283ec8b32d601fc504f
pevec1 <pevec1@yandex.ru>
2023-05-26 20:10:09 +0300

    update 2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,6 +1,7 @@
 let divs = document.querySelectorAll('.has-tooltip')
 let elem = document.createElement('div')
-
+let title = '',
+  elem2
 for (let div of divs) {
   div.addEventListener('focus', () => {
     div.addEventListener('click', e => {
@@ -11,14 +12,13 @@ for (let div of divs) {
       elem.style = 'left: 0; top: 0'
       div.parentNode.insertBefore(elem, div.nextSibling)
       elem.classList.add('tooltip_active')
-      console.log('tooltip active')
-      let isAgain = true
+      title = div.title
+      elem2 = elem
       div.addEventListener('click', e => {
         e.preventDefault()
-        if (isAgain) {
-          elem.classList.remove('tooltip_active')
+        if (title == div.title) {
+          elem.classList.toggle('tooltip_active')
           console.log('tooltip remove')
-          isAgain = false
         }
       })
     })
