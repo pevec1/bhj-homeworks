db73d268ae055431a36792e92c8155653eab2a67
pevec1 <pevec1@yandex.ru>
2023-05-27 16:57:04 +0300

    обновл. решение задачи 1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,26 +1,36 @@
 let divs = document.querySelectorAll('.has-tooltip')
 let elem = document.createElement('div')
 let title = '',
-  elem2
+  elemActive = null
 for (let div of divs) {
-  div.addEventListener('focus', () => {
+  //div.addEventListener('focus', () => {
     div.addEventListener('click', e => {
       e.preventDefault()
+
+      if(elemActive){
+        elemActive.classList.remove('tooltip_active');
+        elemActive = null;
+      }
+
       elem.classList.add('tooltip')
       elem.innerHTML = div.getAttribute('title')
       let w = window.getComputedStyle(div).width
       elem.style = 'left: 0; top: 0'
       div.parentNode.insertBefore(elem, div.nextSibling)
       elem.classList.add('tooltip_active')
-      title = div.title
-      elem2 = elem
-      div.addEventListener('click', e => {
+      console.log('tooltip active')
+      title = elem.textContent
+      elemActive = elem    
+    })
+} 
+   document.addEventListener('click', e => {
         e.preventDefault()
-        if (title == div.title) {
-          elem.classList.toggle('tooltip_active')
+        console.log(e.target.classList.contains('has-tooltip'))
+        if (elemActive&&!e.target.classList.contains('has-tooltip')) {
+          elemActive.classList.remove('tooltip_active')
+          elemActive = null
           console.log('tooltip remove')
         }
       })
-    })
-  })
-}
+
+
