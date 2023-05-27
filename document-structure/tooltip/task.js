fa23f426847b5421321d5ecdfef7912f91aafc36
pevec1 <pevec1@yandex.ru>
2023-05-28 01:15:35 +0300

    update tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,8 +4,11 @@ let title = '',
   elemActive = null
 for (let div of divs) {
   //div.addEventListener('focus', () => {
+    let pos = div.getBoundingClientRect()
     div.addEventListener('click', e => {
       e.preventDefault()
+      let w = div.getBoundingClientRect()
+      console.log(w.bottom, w.left)
 
       if(elemActive){
         elemActive.classList.remove('tooltip_active');
@@ -14,14 +17,36 @@ for (let div of divs) {
 
       elem.classList.add('tooltip')
       elem.innerHTML = div.getAttribute('title')
-      let w = window.getComputedStyle(div).width
-      elem.style = 'left: 0; top: 0'
+      elem.style.cssText = "position:fixed; "
+      elem.style.left = w.left+"px"
+      elem.style.top = w.bottom+"px"
       div.parentNode.insertBefore(elem, div.nextSibling)
       elem.classList.add('tooltip_active')
       console.log('tooltip active')
       title = elem.textContent
-      elemActive = elem    
+      elemActive = elem   
+    div.addEventListener('click', e => {
+      e.preventDefault()
+      console.log(title, elemActive)
+      if (div.title === elem.textContent){
+        elemActive.classList.toggle('tooltip_active')
+        console.log('tooltip toggle')
+
+      }
     })
+    })
+    div.addEventListener('click', e => {
+      e.preventDefault()
+      console.log(div.title, elem)
+      if (div.title !== elem.textContent){
+        elem.textContent = div.title
+
+        elem.classList.add('tooltip_active')
+        console.log('tooltip toggle add2')
+
+      }
+    })
+   
 } 
    document.addEventListener('click', e => {
         e.preventDefault()
@@ -32,5 +57,10 @@ for (let div of divs) {
           console.log('tooltip remove')
         }
       })
-
+      document.addEventListener('scroll', (e) => {
+      if (elemActive&&e.timeStamp> 100) {
+        elemActive.classList.remove('tooltip_active')
+        elemActive = null
+      }
+    })
 
